899333cdb777ca2edd89740075faf6262b18cbb2
PROTACKER <56638837+Hacker-BlackHATS@users.noreply.github.com>
2020-05-14 20:01:22 +0530

    UPDATE
    Open phpMyAdmin.
    Part 1:
              • Create a database (here students)
              • Create a table (here student)
              • Create 5 column(here Roll_no, Name,sub1,sub2,sub3)
    Part 2: Create a .php file with following code:

--- Q9.php
+++ Q9.php
@@ -1,47 +1,45 @@
 <html>
-<head><title>QUESTION-8</title></head>
+<head><title>QUESTION-9</title></head>
 <body>
-
+	<form method="post" action="" >
+		<table border="1" >
+			<tr>
+				<td><label for="Roll_no">Roll Number</label></td>
+				<td><input type="text"
+				name="rno" id="Roll_no"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="Submit"/><td><input type="reset" value="Reset"/>
+			</tr>
+		</table>
+	</form>
 <?php
+
 if(isset($_POST["submit"]) == "submit")
 {
-	$EMAIL =$_POST["EMAILID"];
-
-	$regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/'; 
-	if (preg_match($regex, $EMAIL))
-	{
-        echo $EMAIL . " is a valid email. We can accept it.";
-        $a1 = explode("@",$EMAIL);
-        $a2 = explode(".",$a1[1]);
-        $servername = "localhost";
-        $username = "root";
-        $password = "12345";
-        $dbname = "ques8";
-
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        if ($conn->connect_error)
-        {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        $sql = "SELECT * FROM email where domain ='gmail.com' ";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0)
-        {
-            echo " <br >domain is valid";
-        }
-        elseif ($result->num_rows > 1)
-        {
-            echo "invalid domain";
-        }
-        $conn->close();
-    }
-    else 
-    { 
-        echo $EMAIL . " is an invalid email. Please try again.";
-    } 
+	$id= trim($_POST["rno"]);
+	$cn=mysqli_connect("localhost:3307","root","");
+	
+	// Select the database to use
+	mysqli_select_db($cn,"students");
+	$query = "SELECT * FROM `student` WHERE `Roll no` ='".$id."' ;";
+	$result = mysqli_query($cn,$query) or die(mysqli_error($cn));
+	
+	//Execute the query
+	While($row=mysqli_fetch_array($result)){
+		echo "Roll No:<input type=\"text\" value=".$row['Roll no']."><br>";
+		echo "Name:<input type=\"text\" value=".$row['Name']."><br>";
+		echo "S1:<input type=\"text\" value=".$row['sub1']."><br>";
+		echo "S2:<input type=\"text\" value=".$row['sub2']."><br>";
+		echo "S3:<input type=\"text\" value=".$row['sub3']."><br>";
+		$x=$row['sub1']+$row['sub2']+$row['sub3'];
+		if($x>200){
+			echo "Grade:<input type=\"text\" value=Good><br>";
+		}
+		else{
+			echo "Grade:<input type=\"text\" value=Poor><br>";
+		}
+	}
 }
 ?>
 </body>
